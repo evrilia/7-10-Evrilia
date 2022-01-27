fdfe228f4b10658e8ab1ff431831bdf13981453a
evrilia <evriliaelsyabilaa@gmail.com>
2022-01-27 10:29:33 +0700

    Modul 3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,3 +34,6 @@ Route::get('/gallery', function () {
         "title" => "Gallery"
     ]);
 });
+
+
+Route::resource('/contacts', ContactController::class);
